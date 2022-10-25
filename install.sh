d81a6604f4e306ccabd47130144c36cb48157581
D3vil0p3r <crazysnob@live.it>
2022-10-25 22:56:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-sudo cp -rf c_decorate c_filter c_cleanup commentor /usr/local/bin/
+sudo mkdir -p "/usr/share/commentor"
+sudo mkdir -p "/usr/bin"
+sudo cp -rf c_decorate c_filter c_cleanup commentor /usr/share/commentor/
+sudo chmod 755 /usr/share/commentor/c_decorate /usr/share/commentor/c_filter /usr/share/comment>
+
+cat << EOF | sudo tee "/usr/bin/commentor"
+#!/bin/sh
+exec /usr/share/commentor/commentor "\$@"
+EOF
+
+sudo chmod a+x "/usr/bin/commentor"
 
 echo "Installation ended. Run 'commentor -h'."
